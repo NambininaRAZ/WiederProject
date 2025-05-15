09e71254e6d7af88ea2f2a8a933dc0b60077cb21
lefortnuno <trofelnuno@gmail.com>
2025-05-15 17:05:44 +0100

    fix: daysUI

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()], 
+  server: {
+    // host: "192.168.x.x",
+    host: "192.168.0.191",
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+ 
+ 
